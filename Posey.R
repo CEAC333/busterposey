108068f6afd48d3131d1d8d2c3761e16c7b99996
atse0612 <atse0612@users.noreply.github.com>
2017-03-07 09:52:39 -0800

    Add files via upload

---
+++ Posey.R
@@ -0,0 +1,48 @@
+# Buster Posey
+
+
+### Changing the Working Directory
+
+
+setwd('./Introduction to Data Science UCB/Week 6')
+
+
+### Reading the Data
+
+posey <- read.csv('./posey.csv')
+
+head(posey)
+
+### Checking for any Null or NA Values
+
+any(is.na(posey))
+
+### Presenting the Mean, Median, and Range of Vector
+
+rbi = posey$RBI
+
+
+#### Mean
+
+mean(rbi)
+
+
+### Median
+
+
+median(rbi)
+
+#### Range of the Vector
+
+max(rbi) - min(rbi)
+c(0,21,67,72,103)
+length(c(0,21,67,72,103))
+
+### Regression Analysis
+
+
+
+regr <- lm(wRC. ~ R + H + rbi, posey)
+print(regr)
+summary(regr)
+
